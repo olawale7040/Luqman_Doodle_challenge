99908f2458274d0e73e76947bf3452f61e22bc27
olawale7040 <olawale7040@gmail.com>
2021-11-27 10:13:33 +0100

    Install project dependencies and worked on loginPage

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+// import Chat from "./components/Chat/Chat";
+import Join from "../views/Join/Join";
+
+const routes = [
+  {
+    path: "/",
+    element: <Join />,
+  },
+  //   {
+  //     path: "/chat",
+  //     element: <Chat />,
+  //   },
+];
+export default routes;
